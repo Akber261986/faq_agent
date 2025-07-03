08dd78d7fdfec32e16b99d882d33a9253583e712
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2025-07-03 13:06:50 +0500

    update main and delete proc

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 # main.py
 from fastapi import FastAPI
 from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
-import json
 from agents import Agent, Runner
-import google.generativeai as genai
 from fastapi.middleware.cors import CORSMiddleware
-import asyncio
+import json
 from config import config
-
-
-# Load .env
-load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
 
 app = FastAPI()
 
@@ -34,23 +24,17 @@ faq_knowledge = load_faq_text()
 @app.post("/ask")
 async def ask_faq(question: Question):
     prompt = f"""
-You are an intelligent e-commerce assistant. Use the following FAQ to answer the user's question.
+    You are an intelligent e-commerce assistant. Use the following FAQ to answer the user's question.
 
-FAQ:
-{faq_knowledge}
+    FAQ:
+    {faq_knowledge}
 
-User's Question: {question.question}
-Answer:
-"""
-
-    # model = genai.GenerativeModel("gemini-1.5-flash")  # You can use "gemini-1.5-pro" if preferred
-    # response = model.generate_content(prompt)
-    # answer = response.text.strip()
-
-    # return { "answer": answer }
+    User's Question: {question.question}
+    Answer:
+    """
     agent = Agent(
         name="e-commerce-assistant",
-        instructions="You are an intelligent e-commerce assistant. Use the following FAQ to answer the user's question.",
+        instructions="You are an intelligent e-commerce assistant. Use the FAQ to answer the user's question.",
     )
     result = await Runner.run (agent, prompt, run_config=config)
     return { "answer": result.final_output }
